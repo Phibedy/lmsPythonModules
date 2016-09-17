aee441be489c309294abfb7a4c2dfad5d66de3d6
phibedy <philipp.schmette@gmx.de>
2016-09-17 20:31:03 +0200

    added first test

--- src/test.cpp
+++ src/test.cpp
@@ -2,20 +2,35 @@
 
 #include <Python.h>
 #include <boost/python.hpp>
+#include <string>
+
+void simpleTest(){
+    using namespace boost::python;
+    try{
+        object main_module = import("__main__");
+        object main_namespace = main_module.attr("__dict__");
+
+
+        object ignored = exec("hello = file('hello.txt', 'w')\n"
+                              "hello.write('Hello world!')\n"
+                              "result = 'wrote it'\n"
+                              "hello.close()",
+                              main_namespace);
+
+
+        std::string text = extract<std::string>(main_namespace["result"]);
+        std::cout <<"my Text: "<<text<<std::endl;
+        ignored = exec("result = 5 ** 2", main_namespace);
+        int five_squared = extract<int>(main_namespace["result"]);
+        std::cout <<"result: "<<five_squared<<std::endl;
+    }catch(error_already_set const &){
+        // print all other errors to stderr
+        PyErr_Print();
+    }
+}
 int main(){
     //initialise the python interpreter
     Py_Initialize();
-
-
-    boost::python::object main_module = boost::python::import("__main__");
-    boost::python::object main_namespace = main_module.attr("__dict__");
-
-    boost::python::object ignored = boost::python::exec("hello = file('hello.txt', 'w')\n"
-                          "hello.write('Hello world!')\n"
-                          "hello.close()",
-                          main_namespace);
-
-    boost::python::object result = boost::python::eval("5 ** 2");
-
 }
+
 
